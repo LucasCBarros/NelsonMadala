47f5873fdf0d78bb93a5df7547d19ce9e0a22871
Mauricio Lorenzetti <mauricio@10-40-40-45.0101.ecps.br.iosda.org>
2017-10-09 16:10:58 -0300

    first commit

--- NelsonMandala/ViewController.swift
+++ NelsonMandala/ViewController.swift
@@ -10,8 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var pickerView: UIPickerView!
+    @IBOutlet weak var goButton: UIButton!
+    
+    let time = Array(1...10)
+    var selectedTime:Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        pickerView.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -21,5 +28,25 @@ class ViewController: UIViewController {
     }
 
 
+}
+
+extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return time.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return String(describing: time[row])
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        selectedTime = time[row]
+    }
+    
 }
 
